a86f1375eeb1814c3e56900244f772711921ecc9
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-03-17 19:23:13 +0900

    file delete and display filename

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,12 @@
-//const { execFile } = require("child_process");
-const { exec } = require("child_process");
+const fs = require('fs');
 const express = require("express");
-const router = express.Router();
 const multer = require("multer");
 
-const upload = multer({ dest: "uploadedFiles/" });
+//const { execFile } = require("child_process");
+const { exec } = require("child_process");
 
+const upload = multer({ dest: "uploadedFiles/" });
+const router = express.Router();
 //child process module
 //const fread = require("child_process").execFile;
 const fread = require("child_process").exec;
@@ -17,14 +18,13 @@ router.get("/", function (req, res) {
 //file upload post
 router.post("/uploadFile", upload.single("attachment"), function (req, res) {
     //exec file and callback result page
-    //const fread = execFile(
-       // "main.exe",
-       // [`uploadedFiles/${req.file.filename}`],
+    //const fread = execFile("main.exe",[`uploadedFiles/${req.file.filename}`],function(error,stdout,stderr){
     const fread = exec(`python3 zogori_for_test/main.py uploadedFiles/${req.file.filename}`,function(error,stdout,stderr){
+        fs.unlinkSync(`uploadedFiles/${req.file.filename}`);
 	    console.log("Error  : ", error);
 	    console.log("stderr : ",stderr);
 	    console.log("stdout : ", stdout);
-            res.render("result", { output: stdout });
+        res.render("result", { output: stdout, filesize:req.file.size });
         }
     );
 });
