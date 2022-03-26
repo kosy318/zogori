c80dec469c78e841f76ed4880b326673865d1d9c
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-03-26 21:26:52 +0900

    add pymetrics

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
 const express = require("express");
 const multer = require("multer");
+const bodyParser = require('body-parser');
+
 
 // const { execFile } = require("child_process");
 const { exec } = require("child_process");
+const { request } = require("http");
 
 const upload = multer({ dest: "uploadedFiles/" });
 const router = express.Router();
@@ -19,16 +22,32 @@ router.get("/", function (req, res) {
 router.post("/uploadFile", upload.single("attachment"), function (req, res) {
     //exec file and callback result page
     // const fread = execFile("main.exe",[`uploadedFiles/${req.file.filename}`],function(error,stdout,stderr){
-    const fread = exec(
-        `python3 zogori_for_test/main.py uploadedFiles/${req.file.filename}`,
-        function (error, stdout, stderr) {
-            fs.unlinkSync(`uploadedFiles/${req.file.filename}`);
-            console.log("Error  : ", error);
-            console.log("stderr : ", stderr);
-            console.log("stdout : ", stdout);
-            res.render("result", { output: stdout, filesize: req.file.size });
-        }
-    );
+    let language = req.body.language;
+    console.log(req.body);
+    if(language == "c/c++"){
+        const fread = exec(
+            `python3 zogori_for_test/cppcomplexity.py uploadedFiles/${req.file.filename}`,
+            function (error, stdout, stderr) {
+                fs.unlinkSync(`uploadedFiles/${req.file.filename}`);
+                console.log("Error  : ", error);
+                console.log("stderr : ", stderr);
+                console.log("language : ", language)
+                res.render("result", { output: stdout, filesize: req.file.size });
+            }
+         );
+    }
+    else if(language == "python"){
+        const fread = exec(
+            `python3 zogori_for_test/pycomplexity.py uploadedFiles/${req.file.filename}`,
+            function (error, stdout, stderr) {
+                fs.unlinkSync(`uploadedFiles/${req.file.filename}`);
+                console.log("Error  : ", error);
+                console.log("stderr : ", stderr);
+                console.log("language : ", language)
+                res.render("result", { output: stdout, filesize: req.file.size });
+            }
+         );
+    }
 });
 
 module.exports = router;
