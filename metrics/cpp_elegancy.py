fc4d7a383ae7cb9260406a6318a36967d4aa2e87
kosy318 <kosy1782@gmail.com>
2022-04-29 15:36:51 +0900

    cpp_elegancy.py + ipynb

--- metrics/cpp_elegancy.py
+++ metrics/cpp_elegancy.py
@@ -1,4 +1,11 @@
-def cal_elegancy(complexity):
-    elegancy = 0
+import numpy as np
+
+def cal_elegancy(result):
+    elegancy = result['number_var'] + len(result['distinct_var']) + len(result['distinct_var']) / result[
+        'number_var']
+    elegancy += result['number_func'] + len(result['distinct_func']) + len(result['distinct_func']) / result[
+        'number_func']
+    elegancy += result['LOC']
+    elegancy = 100*pow(elegancy/100, np.log(result['depth']))
 
     return elegancy
