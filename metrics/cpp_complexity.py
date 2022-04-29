5465da44f8b39e2c0caa8fe12712efa8658078a3
kosy318 <kosy1782@gmail.com>
2022-04-29 16:18:57 +0900

    merge cpp_elegancy.py to cpp_complexity.py

--- metrics/cpp_complexity.py
+++ metrics/cpp_complexity.py
@@ -2,14 +2,10 @@ import math
 import ply.lex as lex
 import cpp_tokrules
 import re
+import numpy as np
 import sys
 from collections import deque
-from cpp_elegancy import cal_elegancy
 
-
-# make python exe file
-# on powerShell : pip install pyinstaller
-# on powerShell : pyinstaller -F main.py tokrules.py
 
 def cal_complexity(file_name):
     # Build the lexer
@@ -38,27 +34,6 @@ def cal_complexity(file_name):
             if not enter and not one_line_comment:
                 data += line
                 LOC += 1
-
-    # read_file = sys.argv[1]
-    # with open(read_file, 'r') as file:
-    #     comment_start = False
-    #     comment_end = False
-    #     for line in file.readlines():
-    #         enter = re.fullmatch("[ \t\n]+", line)
-    #         one_line_comment = re.match("^[ \t]*//.*", line)
-    #         if not comment_start:
-    #             comment_start = re.match("^[ \t]*/(\*).*", line)
-    #         comment_end = re.match("^[ \t]*(\*)/.*", line)
-    #         if not comment_end and comment_start:
-    #             continue
-    #         elif comment_end and comment_start:
-    #             comment_end = False
-    #             comment_start = False
-    #             continue
-    #
-    #         if not enter and not one_line_comment:
-    #             data += line
-    #             LOC += 1
 
     # Give the lexer some input
     lexer.input(data)
@@ -160,6 +135,14 @@ def cal_complexity(file_name):
     difficulty = (n_1 / 2) * (N_2 / n_2)
     effort = difficulty * volume
 
+    def cal_elegancy(res):
+        eleg = res['number_var'] + len(res['distinct_var']) + len(res['distinct_var']) / res['number_var']
+        eleg += res['number_func'] + len(res['distinct_func']) + len(res['distinct_func']) / res['number_func']
+        eleg += res['LOC']
+        eleg = 100 * pow(eleg / 100, np.log(res['depth']))
+
+        return eleg
+
     complexity = {
         "distinct_func": set(result['FUNCTION']),
         "distinct_var": set(result['ID']),
@@ -171,20 +154,5 @@ def cal_complexity(file_name):
     }
 
     complexity['elegancy'] = cal_elegancy(complexity)
-
-    # print(f'{result["BRACE"]}')
-    # print(f'''<tr><td>Program vocabulary</td><td>{vocabulary}</td></tr>
-    # <tr><td>Program length</td><td>{length}</td></tr>
-    # <tr><td>Estimated program length</td><td>{estimated_length}</td></tr>
-    # <tr><td>Volume</td><td>{volume}</td></tr>
-    # <tr><td>Difficulty</td><td>{difficulty}</td></tr>
-    # <tr><td>Effort</td><td>{effort}</td></tr>
-    # <tr><td>Functions</td><td>{len(set(result['FUNCTION']))}: {set(result['FUNCTION'])}</td></tr>
-    # <tr><td>total nb of Functions</td><td>{len(result['FUNCTION'])}</td></tr>
-    # <tr><td>Variables</td><td>{len(set(result['ID']))}: {set(result['ID'])}</td></tr>
-    # <tr><td>total nb of Variables</td><td>{len(result['ID'])}</td></tr>
-    # <tr><td>Depth</td><td>{depth}</td></tr>
-    # <tr><td>CCM</td><td>{CCM}</td></tr>
-    # <tr><td>LOC</td><td>{LOC}</td></tr>''')
 
     return complexity
