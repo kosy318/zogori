ca64fc54b5d5a88d856d8204b237fed97273ce6c
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-04-29 17:18:02 +0900

    test

--- metrics/main.py
+++ metrics/main.py
@@ -5,11 +5,15 @@ import sys
 file_name = sys.argv[1]
 file_type = file_name.split('.')[-1]
 
+
+print("file type  : " file_type)
 complexity = {}
 if (file_type == "cpp") or (file_type == "c"):
     complexity = cppcom.cal_complexity(file_name)
+    print("c++ if")
     # print(complexity)
 if file_type == "py":
+    print("python if")
     complexity = pycom.cal_complexity(file_name)
 
 print(f'''<tr><td>Functions</td><td>{len(complexity['distinct_func'])}: {complexity['distinct_func']}</td></tr>
